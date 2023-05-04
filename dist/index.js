8be58123b1e1eea75109f2b9519913d0251274e2
Ben Watkins <hi@bwatkins.dev>
2023-05-03 20:27:25 -0500

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,36 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 784:
+/***/ ((__unused_webpack_module, exports) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.a = void 0;
+const a = async () => {
+    //
+};
+exports.a = a;
+(0, exports.a)();
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__[784](0, __webpack_exports__);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
+/******/ })()
+;
